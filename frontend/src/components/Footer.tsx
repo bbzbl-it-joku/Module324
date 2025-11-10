88fe758f0c7099d52e10e050e9c5319a4f39bfe7
lorenz <lorenz.boss@bbzbl-it.ch>
2025-11-10 14:15:17 +0100

    feat(frontend): Add routing with Home and NotFound pages, and implement Footer component

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,41 @@
+export default function Footer() {
+  return (
+    <footer className="relative p-4 text-center text-sm text-gray-500 dark:text-gray-400">
+      <div className="flex flex-wrap justify-center gap-x-4 gap-y-2">
+        <span>Credits: </span>
+        <a
+          href="https://github.com/lorenzboss"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link-secondary"
+        >
+          lorenzboss
+        </a>
+        <a
+          href="https://github.com/leandroaebi"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link-secondary"
+        >
+          leandroaebi
+        </a>
+        <a
+          href="https://github.com/jokudev"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link-secondary"
+        >
+          jokudev
+        </a>
+        <a
+          href="https://github.com/simiAtschool"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link-secondary"
+        >
+          simiAtschool
+        </a>
+      </div>
+    </footer>
+  );
+}
